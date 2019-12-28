d0c2151e659b0157de47ce488ad1964fff6489a9
Ed Burtnieks <edburtnieks@gmail.com>
2019-12-28 16:57:35 +0200

    Add prettier

--- src/App.js
+++ src/App.js
@@ -3,43 +3,49 @@ import config from './config';
 
 const getToken = async () => {
   try {
-    const response = await fetch(`${config.commercelayerBaseEndpoint}/oauth/token`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        'grant_type': 'client_credentials',
-        'client_id': config.commercelayerClientId,
-        'scope': config.commercelayerScope,
-      }),
-    });
+    const response = await fetch(
+      `${config.commercelayerBaseEndpoint}/oauth/token`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          grant_type: 'client_credentials',
+          client_id: config.commercelayerClientId,
+          scope: config.commercelayerScope,
+        }),
+      }
+    );
 
     const token = await response.json();
 
     return token;
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-}
+};
 
 const getProducts = async (access_token) => {
   try {
-    const response = await fetch(`${config.commercelayerBaseEndpoint}/api/skus`, {
-      headers: {
-        'Accept': 'application/vnd.api+json',
-        'Authorization': `Bearer ${access_token}`,
+    const response = await fetch(
+      `${config.commercelayerBaseEndpoint}/api/skus`,
+      {
+        headers: {
+          Accept: 'application/vnd.api+json',
+          Authorization: `Bearer ${access_token}`,
+        },
       }
-    });
-  
+    );
+
     const products = await response.json();
-  
+
     return products;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 class App extends Component {
   state = {
@@ -56,9 +62,7 @@ class App extends Component {
   render() {
     const {
       isLoading,
-      products: {
-        data,
-      },
+      products: { data },
     } = this.state;
 
     return (
@@ -71,7 +75,10 @@ class App extends Component {
             {data.map((product) => (
               <li key={product.id}>
                 <h2>{product.attributes.name}</h2>
-                <img src={product.attributes.image_url} alt={product.attributes.description} />
+                <img
+                  src={product.attributes.image_url}
+                  alt={product.attributes.description}
+                />
               </li>
             ))}
           </ul>
